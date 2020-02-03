a449305c8cf81e5e9848abf6f7656819c257ad49
NikolayMal <55692364+NikolayMal@users.noreply.github.com>
2020-02-03 12:52:20 +0000

    week 2 code

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -2,17 +2,20 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class HelloProcessing extends PApplet
-{	
-	public void settings()
-	{
+public class HelloProcessing extends PApplet {
+	public void settings() {
 		size(500, 500);
 	}
 
 	public void setup() {
 	}
 
-	/*
+	public void draw() {
+		background(255, 0, 0);
+		triangle(10,100,100,90,200,200);
+	}
+
+/*
 	public void draw() {
 		background(0, 0, 255);
 		stroke(255, 0, 0);
@@ -23,11 +26,11 @@ public class HelloProcessing extends PApplet
 		fill(0, 255, 0);
 		rect(50, 10, 100, 200); // tlx, tly, w, h
 		triangle(10, 10, 100, 90, 200, 200); // vertices	
-	}	
-	*/
+	} */
 
-	int x = 0; // 4 bytes
-	
+	/*int x = 0; // 4 bytes
+
+
 	public void draw()
 	{	
 		
@@ -36,5 +39,10 @@ public class HelloProcessing extends PApplet
 		fill(255);
 		ellipse(x, mouseY, 50, 50);		
 		x ++;
-	}
+
+		if (x == 500)
+		{
+			x = 0;
+		} */
 }
+
